8471acc314fd1bea056e23e556e7da6087ecc55c
ArturKraw <artur.krawczyk64@gmail.com>
2017-11-21 05:27:09 +0100

    Modul 6 task 3 - version 0.3 - running

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,23 +3,17 @@ package com.kodilla.testing.collection;
 import java.util.ArrayList;
 
 public class OddNumbersExterminator {
-    private ArrayList<Integer> array2 = new ArrayList<Integer>();
+    private ArrayList<Integer> exterminate = new ArrayList<Integer>();
     private ArrayList<Integer> array = new ArrayList<Integer>();
+    private ArrayList<Integer> array2 = new ArrayList<Integer>();
+    private int sizeArray2; ;
 
     public OddNumbersExterminator() {
-
-
-    }
-    public ArrayList<Integer> getArray() {
-       return array;
-    }
-    public ArrayList<Integer> getArray2() {
-        return array2;
     }
 
-    public ArrayList<Integer> exterminate (ArrayList<Integer> array) {
+    public void exterminate (ArrayList<Integer> array) {
         this.array = array;
-
+        sizeArray2 = 0;
         switch (array.size()) {
             case (0):
                 break;
@@ -27,16 +21,18 @@ public class OddNumbersExterminator {
                 for (int i = 0; i < array.size(); i++) {
                     if (array.get(i) % 2 == 0) {
                         array2.add(array.get(i));
+                        sizeArray2 = array2.size();
                     }
                 }
         }
-        return getArray2();
-
     }
-
-    public ArrayList<Integer> getExterminate() {
+    public int getSizeArray2 () {
+        return sizeArray2;
+    }
+    public ArrayList<Integer> getArray() {
+        return array;
+    }
+    public ArrayList<Integer> getArray2() {
         return array2;
     }
-
-
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,132 +1,73 @@
 package com.kodilla.testing.collection;
 
+import java.util.*;
+import java.util.ArrayList;
+import java.lang.*;
+import java.io.*;
+import java.time.*;
+
+import com.sun.org.apache.xpath.internal.operations.String;
 import org.junit.*;
 
-import java.util.ArrayList;
-
 public class CollectionTestSuite {
+    private int sizeNumbers;
+
+    public int getEvenNrNumbers() {
+        return evenNrNumbers;
+    }
+    public Integer getLastEvenNumber() {
+        return lastEvenNumber;
+    }
+
+    public ArrayList<Integer> numbers = new ArrayList<Integer>();
+        int evenNrNumbers = 0;
+        int lastEvenNumber = 0;
+
+    public void setSizeNumbers(int sizeNumbers) {
+        this.sizeNumbers = sizeNumbers;
+
+        for (int i = 0; i < sizeNumbers; i++) {
+            numbers.add(i + 5);
+            if (((i + 4) % 2) == 0) {
+                evenNrNumbers++;
+                lastEvenNumber = numbers.get(i);
+            }
+        }
+    }
 
     @Before
     public void before() {
         System.out.println("Test Case: begin");
     }
+
     @After
     public void after() {
         System.out.println("Test Case: end");
     }
-    @BeforeClass
-    public void beforeClass() {
-        System.out.println("Test Suite: begin");
-    }
-    @AfterClass
-    public void afterClass() {
-        System.out.println("Test Suite: end");
-    }
-/*
+
     @Test
     public void testOddNumbersExterminatorEmptyList() {
-
-        int sizeNumbers = 0;
-        ArrayList<Integer> numbers = new ArrayList<Integer>(sizeNumbers); //main array
-        ArrayList<Integer> evenNumbers = new ArrayList<Integer>(sizeNumbers); //result of program (exterminate)
-
         //Given
-        int oddNrNumbers =0;
-        int evenNrNumbers=0;
-        boolean progrCorIndic1 = true;
-        System.out.println("List nr 3t: main control parameters: " + "\n  sizeNumbers: " + sizeNumbers + " evenNrNumbers: " + evenNrNumbers + " oddNrNumbers: " + oddNrNumbers);
-
-        if (sizeNumbers != (evenNrNumbers + oddNrNumbers)) {
-            progrCorIndic1 = false;
-            System.out.println("Error in test program: sizeNumbers != (evenNrNumbers + oddNrNumbers)");
-        }
-
+        ArrayList<Integer> numbers = new ArrayList<Integer>(sizeNumbers); //main array
         OddNumbersExterminator oddNumbExt = new OddNumbersExterminator();
-        oddNumbExt.exterminate(numbers, evenNumbers);
-
-        //When
-        boolean progrCorIndic2 = true;
-        if (evenNumbers.size() != evenNrNumbers) {
-            progrCorIndic2 = false;
-            System.out.println("Something wrong with main program: program =  " + progrCorIndic2);
-        }
+        oddNumbExt.exterminate(numbers);
+        // When
+        setSizeNumbers(0);
         //Then
-
-        if (progrCorIndic1 && progrCorIndic2) {
-            System.out.println("Test for this program done");
-        } else {
-            System.out.println("Program needs improvement(s)");
-        }
+        Assert.assertEquals (0, oddNumbExt.getSizeArray2());
     }
-*/
+
     @Test
     public void testOddNumbersExterminatorNormalList() {
-
-        int sizeNumbers = 5;
-
-        ArrayList<Integer> numbers = new ArrayList<Integer>(sizeNumbers); //main array
-        ArrayList<Integer> evenNumbers = new ArrayList<Integer>(sizeNumbers); //result of program (exterminate)
-        ArrayList<Integer> subNumbers = new ArrayList<Integer>(sizeNumbers); //check array
-
         //Given
-        System.out.println("List nr 1t: numbers in test");
-        for (int i =0 ; i < sizeNumbers ; i=i+1) {
-            numbers.add(i + 4);
-            System.out.println(i + "  " + numbers.get(i));
-        }
-
-        int oddNrNumbers =0;
-        int evenNrNumbers=0;
-        boolean progrCorIndic1 = true;
-
-        //System.out.println("List nr 2t: numbers and control paraameters");
-        int k = 0;
-        for (int i = 0; i < sizeNumbers; i++) {
-            if (numbers.get(i) % 2 == 0) {
-                subNumbers.add(numbers.get(i));
-                k=k+1;
-                evenNrNumbers++;
-            } else {
-                oddNrNumbers++;
-            }
-            //System.out.println(i + "  " + numbers.get(i) + "   " + evenNrNumbers + "  " + oddNrNumbers);
-        }
-        System.out.println("\n" +
-                " Main control parameters: " + "\n" +
-                "sizeNumbers: " + sizeNumbers + " evenNrNumbers: " + evenNrNumbers + " oddNrNumbers: " + oddNrNumbers);
-        if (sizeNumbers != (evenNrNumbers + oddNrNumbers)) {
-            progrCorIndic1 = false;
-            System.out.println("Error in test program: sizeNumbers != (evenNrNumbers + oddNrNumbers)");
-        }
-
+        ArrayList<Integer> numbers = new ArrayList<Integer>(sizeNumbers); //main array
         OddNumbersExterminator oddNumbExt = new OddNumbersExterminator();
-
-            oddNumbExt.exterminate(numbers);
-
-
+        oddNumbExt.exterminate(numbers);
         //When
-        System.out.println("List nr " +
-                "List nr2: subNumbers (even - test before exterminate) and evenNumbers (even from exterminate)");
-        boolean progrCorIndic3 = true;
-        for (int n=0; n < subNumbers.size() ; n++) {
-            if (subNumbers.get(n)!=evenNumbers.get(n)) {
-                progrCorIndic3 = false;
-            }
-            System.out.println(n + " test: " + subNumbers.get(n) + " main: " + evenNumbers.get(n));
-        }
+        sizeNumbers = 100;
 
-
-        boolean progrCorIndic2 = true;
-        if (evenNumbers.size() != evenNrNumbers) {
-            progrCorIndic2 = false;
-            System.out.println("Something wrong with main program: program =  " + progrCorIndic2);
-        }
         //Then
+        Assert.assertEquals (evenNrNumbers, oddNumbExt.getSizeArray2());
 
-        if (progrCorIndic1 && progrCorIndic2 && progrCorIndic3){
-            System.out.println("Test for this program done");
-        } else {
-            System.out.println("Program needs improvement(s)");
-        }
     }
 }
