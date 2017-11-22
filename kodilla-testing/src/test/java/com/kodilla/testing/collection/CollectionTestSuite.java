5cb163cdc3ca876975ed916ba4fa580341361335
ArturKraw <artur.krawczyk64@gmail.com>
2017-11-22 15:46:46 +0100

    Zadanie 6.3 wersja c

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,42 +1,28 @@
 package com.kodilla.testing.collection;
 
-import java.util.*;
 import java.util.ArrayList;
 import java.lang.*;
-import java.io.*;
-import java.time.*;
 
-import com.sun.org.apache.xpath.internal.operations.String;
+//import com.sun.org.apache.xpath.internal.operations.String;
 import org.junit.*;
 
 public class CollectionTestSuite {
     private int sizeNumbers;
+    private int numbersSize=0;
 
-    public int getEvenNrNumbers() {
-        return evenNrNumbers;
-    }
-    public Integer getLastEvenNumber() {
-        return lastEvenNumber;
-    }
-
-    public ArrayList<Integer> numbers = new ArrayList<Integer>();
-        int evenNrNumbers = 0;
-        int lastEvenNumber = 0;
-
-    public void setSizeNumbers(int sizeNumbers) {
-        this.sizeNumbers = sizeNumbers;
-
-        for (int i = 0; i < sizeNumbers; i++) {
-            numbers.add(i + 5);
-            if (((i + 4) % 2) == 0) {
-                evenNrNumbers++;
-                lastEvenNumber = numbers.get(i);
-            }
+    public ArrayList<Integer> collectionTestSuite (int numbersSize) {
+        this.numbersSize=numbersSize;
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < numbersSize; i++) {
+            numbers.add(i + 3);
         }
+        return numbers;
     }
 
+    public int getNumbersSize () {
+        return numbersSize;
+    }
 
-    
     @Before
     public void before() {
         System.out.println("Test Case: begin");
@@ -50,26 +36,24 @@ public class CollectionTestSuite {
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         //Given
-        ArrayList<Integer> numbers = new ArrayList<Integer>(sizeNumbers); //main array
         OddNumbersExterminator oddNumbExt = new OddNumbersExterminator();
-        oddNumbExt.exterminate(numbers);
         // When
-        setSizeNumbers(0);
+        int a =0;
+        oddNumbExt.exterminate(collectionTestSuite(a));
         //Then
         Assert.assertEquals (0, oddNumbExt.getSizeArray2());
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList() {
-        //Given
-        ArrayList<Integer> numbers = new ArrayList<Integer>(sizeNumbers); //main array
+        // Given
         OddNumbersExterminator oddNumbExt = new OddNumbersExterminator();
-        oddNumbExt.exterminate(numbers);
         //When
-        sizeNumbers = 100;
-
+        int a = 101;
+        oddNumbExt.exterminate(collectionTestSuite(a));
         //Then
-        Assert.assertEquals (evenNrNumbers, oddNumbExt.getSizeArray2());
-
+        int evenNr = getNumbersSize()/2;
+        Assert.assertEquals (evenNr , oddNumbExt.getSizeArray2());
     }
+
 }
