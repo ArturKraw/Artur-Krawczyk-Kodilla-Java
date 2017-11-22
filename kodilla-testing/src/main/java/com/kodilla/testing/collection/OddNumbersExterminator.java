b927bf968ce52d9029a8a9c26ace5b06d160158e
ArturKraw <artur.krawczyk64@gmail.com>
2017-11-22 15:46:46 +0100

    Zadanie 6.3 wersja c

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -2,38 +2,26 @@ package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
 
-
-
 public class OddNumbersExterminator {
-    private ArrayList<Integer> exterminate = new ArrayList<Integer>();
     private ArrayList<Integer> array = new ArrayList<Integer>();
     private ArrayList<Integer> array2 = new ArrayList<Integer>();
-    private int sizeArray2; ;
+    private int sizeArray2;
 
-    public OddNumbersExterminator() {
+    public void OddNumbersExterminator (){
     }
 
     public void exterminate (ArrayList<Integer> array) {
-        this.array = array;
-        sizeArray2 = 0;
-        switch (array.size()) {
-            case (0):
-                break;
-            default:
-                for (int i = 0; i < array.size(); i++) {
-                    if (array.get(i) % 2 == 0) {
-                        array2.add(array.get(i));
-                        sizeArray2 = array2.size();
-                    }
-                }
+        for (int i = 0; i < array.size(); i++) {
+            if ((array.get(i) % 2) == 0) {
+                array2.add(array.get(i));
+            }
         }
     }
+
     public int getSizeArray2 () {
-        return sizeArray2;
+        return array2.size();
     }
-    public ArrayList<Integer> getArray() {
-        return array;
-    }
+
     public ArrayList<Integer> getArray2() {
         return array2;
     }
