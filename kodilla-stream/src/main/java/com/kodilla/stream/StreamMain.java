a06fb1f48736c9a84bede7a7cfbbd0e4d5322976
ArturKraw <artur.krawczyk64@gmail.com>
2017-12-08 23:25:47 +0100

    Zadanie 7.5 [wersja 0.1 (działa) do 1. sprawdzenia]

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,25 +1,58 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+
+import com.kodilla.stream.array.ArrayOperations;
+
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
 
 public class StreamMain {
-    public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+    int[] numbers;
+    int n, m;
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+    public static void theAverage (int[] numbers, int n, int m) {
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+    System.out.println("numbers (all): ");
+            IntStream.range(0,numbers.length).
+            forEach (i ->System.out.print(numbers[i]+"  "));
+            System.out.println("\n"+"numbers (selected) ["+n +"] ...["+(m -1)+"]: ");
+            IntStream.range(n,m).
 
+    forEach (i ->System.out.print(numbers[i]+"  "));
+
+    Double average = IntStream.range(n, m)
+            .map(i -> numbers[i]).average().getAsDouble();
+        java.text.DecimalFormat df=new java.text.DecimalFormat("0.00");
+        System.out.println("\n" + "average (of selected): "+df.format(average)  + "\n");
     }
 
+    public static void main(String[] args) {
+
+        int [] numbers = {42, 12, 10, 20, 30, 40, 43, 23, 12, 34, 56, 78, 99, 101};
+        int startInclusive = 2;
+        int endExclusivenint1 = 6;
+        int endExclusivenint;
+        if (endExclusivenint1 <= numbers.length) {
+            endExclusivenint = endExclusivenint1;
+            } else {
+            endExclusivenint = numbers.length;
+        }
+
+        double aver = ArrayOperations.getAverage(numbers, startInclusive, endExclusivenint);
+        java.text.DecimalFormat df=new java.text.DecimalFormat("0.00");
+        System.out.println("\n" + "AverageA: "  + df.format(aver));
+
+    }
 }
+/*
+Zadanie:
+        interfejs ArrayOperations z jedną metodą statyczną - getAverage(int[] numbers).
+        Metoda getAverage(int[] numbers) powinna:
+        - przy pomocy strumienia IntStream oraz metody range(int startInclusive, int endExclusive) wyświetlić kolejne elementy tablicy, a następnie:
+        - przy pomocy drugiego strumienia IntStream oraz metody range(int startInclusive, int endExclusive), a także kolektora average() obliczyć średnią. Na końcu metoda powinna zwracać średnią jako wynik typu double.
+        W katalogu src/test/java utwórz pakiet com.kodilla.stream.array z jednym zestawem testów ArrayOperationsTestSuite oraz jednym testem testGetAverage().
+        Test powinien przygotować tablicę z przykładowymi wartościami typu int i wywołać metodę getAverage(int[] numbers) interfejsu ArrayOperations.
+        Przy pomocy asercji sprawdź, czy obliczona średnia jest poprawna.
+        */
--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,20 +1,29 @@
 package com.kodilla.stream.array;
 
+
+import java.util.stream.IntStream;
+
+
 public interface ArrayOperations {
 
-    public static int getAverage(int[] numbers) {
 
-        int[] numbersA= numbers.IntStream()
-                .range(int startInclusive, int endExclusive)
-                .toList();
+    static double getAverage(int[] numbers, int startInclusive, int endExclusiven ) {
+        java.text.DecimalFormat df=new java.text.DecimalFormat("0.00");
+        System.out.println("numbers (all): from.[" + 0+ "] ...to.[" + (numbers.length-1) +"]: ");
+        IntStream.range( 0, numbers.length).forEach(i -> System.out.print(numbers[i] + "  "));
+        Double average1 = IntStream.range( 0, numbers.length)
+                .map(i -> numbers[i]).average().getAsDouble();
+        System.out.print("\n" + "average (of all): " + df.format(average1)+ "\n");
 
-        System.out.println(numbersA);
+        System.out.println("numbers (selected): from.[" + startInclusive + "] ...to.[" + (endExclusiven-1) +"]: ");
+        IntStream.range( startInclusive, endExclusiven).forEach(i -> System.out.print(numbers[i] + "  "));
+        Double average2 = IntStream.range( startInclusive, endExclusiven)
+                .map(i -> numbers[i]).average().getAsDouble();
+        System.out.print("\n" + "average (of selected): " + df.format(average2) + "\n");
 
-        double averageA = numbersA.IntStream
-                .range(int startInclusive, int endExclusive)
-                .average()
-                to double
-
-        return averageA;
+        return average2;
     }
 }
+
+
+
