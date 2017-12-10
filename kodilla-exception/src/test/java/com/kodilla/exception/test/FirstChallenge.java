8e9e6db29cbf94a4184b954f86238154edc2c6a6
ArturKraw <artur.krawczyk64@gmail.com>
2017-12-10 22:52:35 +0100

    Modul 8 - Zadanie 8.2 (wersja 0.2 - działa - do sprawdzenia)

--- kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,51 +1,36 @@
 package com.kodilla.exception.test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
-//Wykorzystując blok try-catch-finally obsłuż rzucany wyjątek.
 public class FirstChallenge {
-    private double result, result1;
 
-    public double divide(double a, double b)  {
-        double result;
-        System.out.println("double method");
-        result = 0;
-        try {
-            result = a * 1000000000 / b;
-           System.out.println("try - section");
-            result = (a * 1000000000) / b;
-            System.out.println("(a): " + a + "; (b): " + b + "  ; (a / b) = " + result);
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
         }
-        catch (ArithmeticException e) {
-            System.out.println("catch ArExc- section");
-            System.out.println("Oh no ! It is forbidden tot divide by ZERO ! Error: " + e);
-            System.out.println("Division by zero exception");
-        }
-        catch (Exception e) {
-            System.out.println("catch Exc - section");
-            System.out.println("Exception occurred: " + e);
-        }
-        finally {
-            System.out.println("finally - section");
-            System.out.println("That's it");
-
-        }
-        //System.out.println("result1; " + result1);
-        return result;
+        return a / b;
     }
-     /**
+
+    /**
      * This main can throw an ArithmeticException!!!
      * @param args
      */
     public static void main(String[] args) {
-
+        double a = 3.0;
+        double b = 4.0; // change this to ZERO to check the progam
+        double result;
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(2000000000, 0);
-
-        System.out.println("(Main program) result: " +result);
+        try {
+            System.out.println("\n" + "try section");
+            result = firstChallenge.divide(a, b);
+            System.out.println("(a): " + a + "; (b): " + b + "  ; (a / b) = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("\n" + "catch section");
+            System.out.println("Division by zero exception");
+            System.out.println("Oh no ! It is forbidden to divide by ZERO ! Error: " + e);
+        }
+        finally {
+            System.out.println("\n" + "finally section");
+            System.out.println("That's it");
+        }
     }
 }
