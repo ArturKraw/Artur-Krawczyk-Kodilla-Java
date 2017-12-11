1f4cb34330bd62749e57c3733ccc4976f90e6f68
ArturKraw <artur.krawczyk64@gmail.com>
2017-12-11 20:27:42 +0100

    Modul 8 - Zadanie 8.3 (wersja 0.2 - poprawki po 1. sprawdzeniu)

--- kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/ExceptionHandling.java
@@ -8,10 +8,9 @@ public class ExceptionHandling {
         // It is possible to change x and(or) y
         // Exception if( x<1 or x>=2 or y==1.5)
         double x = 2.5;
-        double y = 2.5;
+        double y = 1.5;
 
         SecondChallenge challenge = new SecondChallenge();
-        SecondChallengeException challengeEx = new SecondChallengeException();
 
         try {
             System.out.println("\n" + "try section - ExceptionHandling");
@@ -20,7 +19,7 @@ public class ExceptionHandling {
         } catch (SecondChallengeException e) {
             System.out.println("\n" + "catch section - ExceptionHandling");
             System.out.println("Problem with input data (x,y) - out of range !");
-            challengeEx.SecondChallengeExceptionMet();
+            e.SecondChallengeExceptionMet();
 
         } finally {
             System.out.println("\n" + "finally section - ExceptionHandling");
