30c8134a73495fae506adac821f33d7d11c1971a
ArturKraw <artur.krawczyk64@gmail.com>
2017-12-18 10:01:43 +0100

    Modul Spring - Zadanie 10.1 (działa - wersja 0.2 - do 1. sprawdzenia (mala korekta)

--- kodilla-spring/src/test/java/com/kodilla/spring/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -3,10 +3,13 @@ package com.kodilla.spring.com.kodilla.spring.forum;
 import com.kodilla.spring.forum.ForumUser;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
+//@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ForumUserTestSuite {
 
 
