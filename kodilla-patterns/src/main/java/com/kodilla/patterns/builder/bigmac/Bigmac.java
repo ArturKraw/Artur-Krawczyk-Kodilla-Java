074542f80ac72792b5354bf4ce5983851d3cb03e
ArturKraw <artur.krawczyk64@gmail.com>
2017-12-28 22:49:30 +0100

    Modul 11 - Zadanie 11.4 (wersja 0.1) (w trakcie opracowywania)

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import java.util.List;
+
+public final class Bigmac {  //Pizza
+    private final String roll;  //bottom;
+    private final String burgers;  //bottom;
+    private final String sauce;
+    private final List<String> ingredients;
+}
